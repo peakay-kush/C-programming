61e46968759393f2c1e8e9a2626b046df153086d
peakay-kush <101639565+peakay-kush@users.noreply.github.com>
2022-04-19 12:25:32 +0300

    Add files via upload

---
+++ for loop and sum of first 10 integers.c
@@ -0,0 +1,18 @@
+//C program to show the sum of the first 10 integers in for loop
+#include<stdio.h>
+void main()
+{
+	int n,sum;
+	printf("first 10 natural numbers\n");
+	for(n=1; n<=10; n++)
+	{
+		printf("%d\n",n);
+		
+	}
+	
+	sum +=n;
+	{
+			
+	printf("the sum is :%d\n",sum);
+	}
+}
---
+++ for loop.c
@@ -0,0 +1,12 @@
+//C program to display the first ten numbers using for loop
+#include<stdio.h>
+void main()
+{
+	int i;
+	printf("first 10 natural numbers \n");
+	for(i=1; i<=10; i++)
+	{
+		printf("%d\n",i);
+	}
+	
+}
